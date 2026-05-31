811f7f68cc63442dd801fa13c19cc0af338c3e06
norwgwoods-iti <norwgwoods@gmail.com>
2026-05-31 23:12:48 +0300

    Create authorization.py

--- function/authorization.py
+++ function/authorization.py
@@ -1,27 +1,45 @@
 from base.base_class import Base
 
+
 class Authorization(Base):
     def __init__(self):
         super().__init__()
+        self.login = None
 
-    def input_login(self, login):
-        if login.lower() in self.get_list_logins():
-            print('Логин совпалоу')
+    # def input_login(self):
+    #     login = input('Введите Логин: ').lower()
+    #
+    # """Ввод логина"""
+    #
+    # def input_login(self):
+    #     while True:
+    #         login = input('Введите Логин: ').lower()
+    #         if login == self.check_login(login):
+    #             print('Логин введен верно')
+    #             return login
+    #         else:
+    #             print('Такого логина не существует')
+    #             continue
+
+
+    """Функция"""
+    def input_password(self, login):
+        self.login = login
+        print(f'Логин введен верно: {login}')
+        password = input('Введите Пароль: ')
+        if password == self.check_password(login):
+            print('Пароль верный')
         else:
-            print('Логин не совпалоу')
+            print('Пароль не совпадает')
 
 
-    def input_password(self, login, password):
-        if password == self.get_list_password(login):
-            print('Пароль совпалоу')
-        else:
-            print('Пароль не совпалоу')
 
+    # Methods
 
     def authorization(self):
         self.connection_db()
         self.get_cursor_db()
-        self.input_login(input())
+        self.input_password(self.input_login())
         self.close_db()
-
+        print('Авторизация прошла успешно')
 
