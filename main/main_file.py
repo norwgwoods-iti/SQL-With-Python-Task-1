b0c5f4a83a174dd9d66ca3e8a3e898ff9ba2e85d
norwgwoods-iti <norwgwoods@gmail.com>
2026-05-31 22:56:38 +0300

    Add main_file.py

---
+++ main/main_file.py
@@ -0,0 +1,24 @@
+from function.authorization import Authorization
+from function.reset_password import ResetPassword
+from function.registration import Registration
+
+print('Введите число:')
+while True:
+    try:
+        select_function = int(input('1 - Регистрация\n2 - Авторизация\n3 - Сброс пароля\n'))
+        if select_function == 1:
+            reg = Registration()
+            reg.registration()
+        elif select_function == 2:
+            aut = Authorization()
+            aut.authorization()
+        elif select_function == 3:
+            rp = ResetPassword()
+            rp.reset_password()
+        else:
+            print('Число, которое необходимо ввести содержит значения от 1 до 3')
+    except ValueError:
+        print('Необходимо ввести числовое значение от 1 - 3')
+
+
+
