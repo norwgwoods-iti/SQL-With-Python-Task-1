530779bb5cb5346fa1b014449f7a0e08ea0eb50c
norwgwoods-iti <norwgwoods@gmail.com>
2026-05-31 23:12:13 +0300

    Edit running = True in main_file.py

--- main/main_file.py
+++ main/main_file.py
@@ -3,22 +3,25 @@ from function.reset_password import ResetPassword
 from function.registration import Registration
 
 print('Введите число:')
-while True:
+running = True
+while running:
     try:
         select_function = int(input('1 - Регистрация\n2 - Авторизация\n3 - Сброс пароля\n'))
         if select_function == 1:
             reg = Registration()
             reg.registration()
+            running = False
         elif select_function == 2:
             aut = Authorization()
             aut.authorization()
+            running = False
         elif select_function == 3:
             rp = ResetPassword()
             rp.reset_password()
+            running = False
         else:
             print('Число, которое необходимо ввести содержит значения от 1 до 3')
     except ValueError:
         print('Необходимо ввести числовое значение от 1 - 3')
-
 
 
