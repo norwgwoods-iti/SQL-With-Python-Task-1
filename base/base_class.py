fcd6cd1598d55bde1f90a10867be95ba4ec14fe1
norwgwoods-iti <norwgwoods@gmail.com>
2026-06-01 20:02:12 +0300

    Fix base_class.py

--- base/base_class.py
+++ base/base_class.py
@@ -1,93 +1,60 @@
 import sqlite3 as sql
 from pathlib import Path
 
-
 class Base:
+
     def __init__(self):
+        """Присваем None, чтобы потом в функции задать значение и использовать в других методах"""
         self.db = None
         self.cur = None
-        self.sql_path = str(Path(__file__).resolve().parent.parent / 'db' / 'registration.db')
+        self.sql_path = Path(__file__).resolve().parent.parent / 'db' / 'registration.db'
 
     """Возвращаем db"""
     def connection_db(self):
         self.db = sql.connect(self.sql_path)
-        db_name = Path(self.sql_path).name
-
-        print(f'Подключились к Базе Данных - {db_name}')
+        """print для тестов"""
+        # print(f'Подключились к Базе Данных - {self.sql_path.name}')
         return self.db
 
 
     """Возвращаем курсор"""
     def get_cursor_db(self):
-        self.cur = self.db.cursor()
-        return self.cur
+        try:
+            self.cur = self.db.cursor()
+            return self.cur
+        except AttributeError:
+            print('Сначала необходимо подключиться к БД с помощью connection_db()')
+            return None
 
 
     """Получаем все поля таблицы"""
     def get_fields(self):
         try:
             self.cur.execute("SELECT * FROM users_data")
-
             print('Список всех полей таблицы:')
             return self.cur.fetchall()
         except AttributeError:
             print('Сначала необходимо подключиться к БД с помощью connection_db()')
-            return []
+            return None
 
 
-    """Получаем список логинов"""
-    def get_list_logins(self):
+    def get_user_data(self, login):
         self.cur.execute(
             """
-            SELECT Login
+            SELECT Password, Code
             FROM users_data
-            """
-        )
-
-        login_list = []
-
-        login_db = self.cur.fetchall()
-
-        for login in login_db:
-            login_list.append(login[0].lower())
-
-        return login_list
-
-
-    """Получаем Пароль по Логину"""
-    def get_list_password(self, login):
-        self.cur.execute(
-            """
-                SELECT Password
-                FROM users_data
-                WHERE Login = ?;
+            WHERE LOWER (Login) = LOWER(?);
             """, (login,)
         )
 
-        password_db = self.cur.fetchone()
-
-        return password_db[0]
-
-
-    """Получаем Код по Логину"""
-    def get_list_code(self, login):
-        self.cur.execute(
-            """
-                SELECT Code
-                FROM users_data
-                WHERE Login = ?;
-            """, (login,)
-        )
-
-        code_db = self.cur.fetchone()
-
-        return code_db[0]
+        return self.cur.fetchone()
 
 
     """Закрываем БД"""
     def close_db(self):
         if self.db:
             self.db.close()
-            print('База Данных закрыта')
+            """print для тестов"""
+            # print('База Данных закрыта')
         else:
             print('Соединение не было открыто')
