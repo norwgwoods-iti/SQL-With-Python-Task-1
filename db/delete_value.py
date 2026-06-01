e46832f0c623ce3f5a28eb4a93dde94358b7ced6
norwgwoods-iti <norwgwoods@gmail.com>
2026-06-01 20:03:56 +0300

    Add delete_table.py

---
+++ db/delete_value.py
@@ -0,0 +1,20 @@
+import sqlite3 as sql
+from pathlib import Path
+
+path_sql = Path(__file__).resolve().parent / 'registration.db'
+
+db = sql.connect(path_sql)
+
+print(f'Подключились к Базе Данных - {path_sql.name}')
+
+cur = db.cursor()
+
+cur.execute(
+    """
+    DELETE FROM users_data
+    WHERE UserID = 11;
+    """
+)
+print('Строка удалена')
+
+db.commit()
