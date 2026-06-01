59ee1501242b77c37cc36b35622613a70c2429a7
norwgwoods-iti <norwgwoods@gmail.com>
2026-06-01 20:02:21 +0300

    Add create_bd.py

--- db/create_bd.py
+++ db/create_bd.py
@@ -1,11 +1,11 @@
 import sqlite3 as sql
 from pathlib import Path
 
-path_sql = str(Path(__file__).resolve().parent.parent / 'db' / 'registration.db')
+path_sql = Path(__file__).resolve().parent.parent / 'db' / 'registration.db'
 
 db = sql.connect(path_sql)
 
-print(f'Подключились к Базе Данных - {path_sql.split("/")[-1]}')
+print(f'Подключились к Базе Данных - {path_sql.name}')
 
 cur = db.cursor()
 
